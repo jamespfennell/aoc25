f9470851d4bc75ac3a09e2c44f3ab22993373c6e
James Fennell <jamespfennell@gmail.com>
2025-12-08 22:21:15 -0500

    Problem 7, day 1

--- day7.zig
+++ day7.zig
@@ -7,8 +7,34 @@ pub fn problem_1() !u64 {
 }
 
 fn problem_1_impl(file_content: []const u8) !u64 {
-    _ = file_content;
-    return 0;
+    var lines = std.mem.splitSequence(u8, file_content, "\n");
+
+    var active = std.ArrayList(bool).init(std.heap.page_allocator);
+    const first_line = lines.next().?;
+    for (first_line) |c| {
+        try active.append(c == 'S');
+    }
+    var num_splits: u64 = 0;
+    while (lines.next()) |line| {
+        var next_active = active.items[0];
+        for (line, 0..) |c, i| {
+            const this_active = next_active;
+            if (i + 1 < active.items.len) {
+                next_active = active.items[i + 1];
+            }
+            if (this_active and c == '^') {
+                num_splits += 1;
+                if (i > 0) {
+                    active.items[i - 1] = true;
+                }
+                active.items[i] = false;
+                if (i + 1 < active.items.len) {
+                    active.items[i + 1] = true;
+                }
+            }
+        }
+    }
+    return num_splits;
 }
 
 pub fn problem_2() !u64 {
@@ -22,14 +48,14 @@ fn problem_2_impl(file_content: []const u8) !u64 {
 }
 
 test "problem_1_example" {
-    const input = "";
-    try std.testing.expectEqual(0, problem_1_impl(input));
+    const input = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n";
+    try std.testing.expectEqual(21, problem_1_impl(input));
 }
 test "problem_1" {
-    try std.testing.expectEqual(0, problem_1());
+    try std.testing.expectEqual(1499, problem_1());
 }
 test "problem_2_example" {
-    const input = "";
+    const input = ".......S.......\n...............\n.......^.......\n...............\n......^.^......\n...............\n.....^.^.^.....\n...............\n....^.^...^....\n...............\n...^.^...^.^...\n...............\n..^...^.....^..\n...............\n.^.^.^.^.^...^.\n...............\n";
     try std.testing.expectEqual(0, problem_2_impl(input));
 }
 test "problem_2" {
