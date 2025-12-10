d1e0838f2ddb4446c2caa502dde2cd71c97b5af5
James Fennell <jamespfennell@gmail.com>
2025-12-10 07:26:56 -0500

    Problem 8, day 2

--- day8.zig
+++ day8.zig
@@ -77,7 +77,53 @@ pub fn problem_2() !u64 {
 }
 
 fn problem_2_impl(file_content: []const u8) !u64 {
-    _ = file_content;
+    const points = try parse_input(file_content);
+    var point_to_cluster = std.AutoHashMap(u64, u64).init(std.heap.page_allocator);
+    var clusters = std.AutoHashMap(u64, u64).init(std.heap.page_allocator);
+    for (points.items, 0..) |_, i| {
+        try point_to_cluster.put(i, i);
+        try clusters.put(i, 1);
+    }
+
+    var pairs = std.ArrayList(Pair).init(std.heap.page_allocator);
+    var i: usize = 0;
+    while (i + 1 < points.items.len) : (i += 1) {
+        var j: usize = i + 1;
+        while (j < points.items.len) : (j += 1) {
+            const a = points.items[i];
+            const b = points.items[j];
+            try pairs.append(Pair{
+                .a = i,
+                .b = j,
+                .d_squared = a.d_squared(b),
+            });
+        }
+    }
+    std.mem.sort(Pair, pairs.items, {}, compare_pairs);
+
+    i = 0;
+    while (true) : (i += 1) {
+        const pair = pairs.items[i];
+        const cluster_a = point_to_cluster.get(pair.a).?;
+        const cluster_b = point_to_cluster.get(pair.b).?;
+        if (cluster_a == cluster_b) {
+            continue;
+        }
+        if (clusters.count() == 2) {
+            const point_a = points.items[pair.a];
+            const point_b = points.items[pair.b];
+            return point_a.x * point_b.x;
+        }
+        // merge cluster_a and cluster_b
+        try clusters.put(cluster_a, clusters.get(cluster_a).? + clusters.get(cluster_b).?);
+        _ = clusters.remove(cluster_b);
+        var iterator = point_to_cluster.iterator();
+        while (iterator.next()) |entry| {
+            if (entry.value_ptr.* == cluster_b) {
+                entry.value_ptr.* = cluster_a;
+            }
+        }
+    }
     return 0;
 }
 
@@ -125,8 +171,8 @@ test "problem_1" {
     try std.testing.expectEqual(32103, problem_1());
 }
 test "problem_2_example" {
-    const input = "";
-    try std.testing.expectEqual(0, problem_2_impl(input));
+    const input = "162,817,812\n57,618,57\n906,360,560\n592,479,940\n352,342,300\n466,668,158\n542,29,236\n431,825,988\n739,650,466\n52,470,668\n216,146,977\n819,987,18\n117,168,530\n805,96,715\n346,949,466\n970,615,88\n941,993,340\n862,61,35\n984,92,344\n425,690,689";
+    try std.testing.expectEqual(25272, problem_2_impl(input));
 }
 test "problem_2" {
     try std.testing.expectEqual(0, problem_2());
